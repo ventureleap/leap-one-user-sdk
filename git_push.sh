ac37aaad22d9875573511e97cabc310723ef4bcc
damilare <damilare.durojaye@gmail.com>
2020-09-22 11:34:46 +0200

    c68119b90fb00bb75749c45e73c9896ecec76762

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="8e840809e5c076ed336956c1bae64f925d0a467c"
+    release_note="c68119b90fb00bb75749c45e73c9896ecec76762"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
