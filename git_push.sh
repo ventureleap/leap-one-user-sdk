98178ff412fa5619f3b323e711bc2e53f69ae8dd
GitHub Action <action@github.com>
2021-02-19 02:37:52 +0000

    User Service commit hash: 12444f0

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="User Service commit hash: a2e8faf"
+    release_note="User Service commit hash: 12444f0"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
